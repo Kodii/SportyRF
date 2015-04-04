44e9703953e6027cdcee93e433f9d6b0a63983f7
Kodii <kordian.stryczek@gmail.com>
2015-04-04 18:04:59 +0200

    merge

--- src/main/java/gui/database/Insert.java
+++ src/main/java/gui/database/Insert.java
@@ -26,6 +26,17 @@ public class Insert extends DatabaseConnection {
             insertEvent.setInt(6, startType);
 
             insertEvent.executeUpdate();
+            
+            insertEvent = connection.prepareStatement("INSERT INTO START_TYPE(id, name) VALUES(?,?)");
+            insertEvent.setInt(1, competition_id);
+            if(startType == 1){
+            	insertEvent.setString(2, "WSPOLNY");
+            }
+            if(startType == 2){
+            	insertEvent.setString(2, "INDYWIDUALNY");
+            }
+            insertEvent.executeUpdate();
+            
             connection.commit();
             connection.close();
 
@@ -68,6 +79,51 @@ public class Insert extends DatabaseConnection {
     		insertEvent.setString(10, height);
     		insertEvent.setInt(11, category);
     		insertEvent.setInt(12, startNumber);
+    		insertEvent.executeUpdate();
+    		
+    		// adding gender
+    		insertEvent = connection.prepareStatement("INSERT INTO Gender(id, gender) VALUES(?,?)");
+    		insertEvent.setInt(1, competitior_id);
+    		if(gender == 1){
+    			insertEvent.setString(2, "MEZCZYZNA");
+    		}
+    		if(gender == 2){
+    			insertEvent.setString(2, "KOBIETA");
+    		}
+    		insertEvent.executeUpdate();
+    		
+    		// adding team
+    		insertEvent = connection.prepareStatement("INSERT INTO Teams(id, name) VALUES(?,?)");
+    		insertEvent.setInt(1, competitior_id);
+    		if(team == 1){
+    			insertEvent.setString(2, "CZERWONI");
+    		}
+    		if(team == 2){
+    			insertEvent.setString(2, "NIEBIESCY");
+    		}
+    		if(team == 3){
+    			insertEvent.setString(2, "ZIELONI");
+    		}
+    		insertEvent.executeUpdate();
+    		
+    		// adding category
+    		insertEvent = connection.prepareStatement("INSERT INTO Category(id, name) VALUES(?,?)");
+    		insertEvent.setInt(1, competitior_id);
+    		if(category == 1){
+    			insertEvent.setString(2, "DO 25");
+    		}
+    		if(category == 2){
+    			insertEvent.setString(2, "DO 35");
+    		}
+    		if(category == 3){
+    			insertEvent.setString(2, "DO 45");
+    		}
+    		if(category == 4){
+    			insertEvent.setString(2, "DO 55");
+    		}
+    		if(category == 5){
+    			insertEvent.setString(2, "ELITA");
+    		}
     		insertEvent.executeUpdate();
     		
     		connection.commit();
